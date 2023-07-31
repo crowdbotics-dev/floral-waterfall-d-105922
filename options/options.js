3413c2b8e192c033ad1b5b340d1fbc8b81fe333e
Crowdbotics <engineering@crowdbotics.com>
2023-07-31 23:45:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-waterfall-d-105922",
+  url: "https://floral-waterfall-d-105922.botics.co",
+  api: "https://floral-waterfall-d-105922.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
